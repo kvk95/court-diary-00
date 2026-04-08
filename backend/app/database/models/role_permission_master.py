4333f051a9c963942f45522ef51918afcda138d1
kvk95 <kvk95@yahoo.com>
2026-04-08 22:56:30 +0530

    create _ROLE_PERMISSION_MATRIX while signup

---
+++ backend/app/database/models/role_permission_master.py
@@ -0,0 +1,55 @@
+"""role_permission_master"""
+
+from sqlalchemy import ForeignKey, Boolean, CHAR, Integer, String
+from sqlalchemy.orm import relationship, backref
+from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy.sql import func, text
+from typing import Any, Optional
+
+from app.database.models.base.base_model import BaseModel
+
+class RolePermissionMaster(BaseModel):
+    __tablename__ = 'role_permission_master'
+
+    # id : INTEGER
+    id: Mapped[int] = mapped_column(Integer, primary_key=True, autoincrement=True, nullable=False)
+
+    # role_name : VARCHAR(80) COLLATE "utf8mb4_unicode_ci"
+    role_name: Mapped[str] = mapped_column(String(80), nullable=False)
+
+    # module_code : CHAR(8) COLLATE "utf8mb4_unicode_ci"
+    module_code: Mapped[str] = mapped_column(CHAR(8), ForeignKey("refm_modules.code"), nullable=False)
+
+    # allow_all_ind : TINYINT
+    allow_all_ind: Mapped[Optional[bool]] = mapped_column(Boolean, default=False)
+
+    # read_ind : TINYINT
+    read_ind: Mapped[Optional[bool]] = mapped_column(Boolean, default=True)
+
+    # write_ind : TINYINT
+    write_ind: Mapped[Optional[bool]] = mapped_column(Boolean, default=False)
+
+    # create_ind : TINYINT
+    create_ind: Mapped[Optional[bool]] = mapped_column(Boolean, default=False)
+
+    # delete_ind : TINYINT
+    delete_ind: Mapped[Optional[bool]] = mapped_column(Boolean, default=False)
+
+    # import_ind : TINYINT
+    import_ind: Mapped[Optional[bool]] = mapped_column(Boolean, default=False)
+
+    # export_ind : TINYINT
+    export_ind: Mapped[Optional[bool]] = mapped_column(Boolean, default=False)
+
+    # FORWARD RELATIONSHIPS ------------------------------------------------------------
+    # A forward relationship is defined in the table that contains the foreign key.
+
+    # role_permission_master.module_code -> refm_modules.code
+    role_permission_master_module_code_refm_modules = relationship(
+        "RefmModules",
+        foreign_keys=[module_code], 
+        backref=backref("role_permission_master_module_code_refm_moduless", cascade="all, delete-orphan")
+    )
+
+    # FORWARD RELATIONSHIPS ------------------------------------------------------------
+
